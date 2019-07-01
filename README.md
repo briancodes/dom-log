73af03c99ec916497e1aa045ac1cf983e7de44c7
Brian B <15702512+briancodes@users.noreply.github.com>
2019-07-01 19:14:56 +0100

    Feature/dom log (#3)
    * Export an object with functions applied
    
    * Refactoring
    
    * Refactoring
    
    * Error logging if sideBySide param is <= 0
    
    * setLogToConsole default param true
    
    * npm package info update
    
    * Added url
    
    * Major refactoring
    - stringify to own file
    - applying css via objects and Object.entries
    
    * Update demo
    
    * Classes added for users to override styles if needed (with !important)
    
    * Scroll into view

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# @bcodes/dom-log
-
-#### Notes
-
-- `parcel-bundler` example setup https://macwright.org/2018/11/26/small-modules.html
+# dom-log
--- app/app-index.js
+++ app/app-index.js
@@ -1,4 +1,26 @@
-import { lib } from '../lib/index';
+import { domLog } from '../lib/index';
 
 console.log('app-index.js parsed 👌');
-console.log('app-index - lib check:', lib());
+console.log('app-index - lib check:', domLog);
+
+domLog.setLogToConsole(true).applyBackgroundAll();
+
+domLog.setElementId('app-id');
+
+const a = [null, 2, 3, 4, undefined, -Infinity];
+const b = a.map(x => x * 10);
+const c = b.map(x => '' + x);
+
+domLog.log(a);
+domLog.log(b);
+
+domLog
+  .log(b)
+  .log(c)
+  .sideBySide(2);
+
+domLog
+  .log(a)
+  .log(b)
+  .log(c)
+  .sideBySide(3);
